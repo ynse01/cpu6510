576f2d0e9d1c637a5d3f4f1d87e6a81cd05535ec
Ynse Hoornenborg <github@ynse.nl>
2021-03-03 22:10:26 +0100

    Zero page addressing unit tests

--- spec/addressing/zero-page-indexed-x-addressing.spec.ts
+++ spec/addressing/zero-page-indexed-x-addressing.spec.ts
@@ -1,30 +1,33 @@
 import { ByteHelper } from "../../src/byte-helper";
-import { ZeroPageAddressing } from "../../src/cpu/addressing/zero-page-addressing";
+import { ZeroPageIndexedXAddressing } from "../../src/cpu/addressing/zero-page-indexed-x-addressing";
 import { Processor } from "../../src/cpu/processor";
 import { AddressOperationStub } from "./address-operation-stub";
 
 const cpu = new Processor();
 
-describe('ZeroPageAddressing', ()=> {
+describe('ZeroPageIndexedXAddressing', ()=> {
     it('ShouldReadZeroPageAddressFromInstructionStack', ()=> {
         // Arrange
         const programAddress = 0x1234;
         const address = 0x0076;
         const operation = new AddressOperationStub(cpu);
-        const addressing = new ZeroPageAddressing(cpu, operation);
+        const addressing = new ZeroPageIndexedXAddressing(cpu, operation);
         cpu.programCounter = programAddress;
         cpu.memory.write(programAddress, ByteHelper.clipByte(address));
         const value = 130;
         operation.setValueToBeWritten(value);
+        cpu.registerX = 0x12;
+        const expected = 0x0088;
         // Act
         addressing.execute();
         // Assert
         const actual = operation.lastAddress;
-        expect(actual).toBe(address);
-        expect(cpu.memory.read(address)).toBe(value);
+        expect(actual).toBe(expected);
+        expect(cpu.memory.read(expected)).toBe(value);
         // Cleanup
         cpu.reset();
         cpu.memory.write(programAddress, 0);
         cpu.memory.write(address, 0);
+        cpu.memory.write(expected, 0);
     });
 });
--- spec/addressing/zero-page-indexed-y-addressing.spec.ts
+++ spec/addressing/zero-page-indexed-y-addressing.spec.ts
@@ -1,30 +1,33 @@
 import { ByteHelper } from "../../src/byte-helper";
-import { ZeroPageAddressing } from "../../src/cpu/addressing/zero-page-addressing";
+import { ZeroPageIndexedYAddressing } from "../../src/cpu/addressing/zero-page-indexed-y-addressing";
 import { Processor } from "../../src/cpu/processor";
 import { AddressOperationStub } from "./address-operation-stub";
 
 const cpu = new Processor();
 
-describe('ZeroPageAddressing', ()=> {
+describe('ZeroPageIndexedYAddressing', ()=> {
     it('ShouldReadZeroPageAddressFromInstructionStack', ()=> {
         // Arrange
         const programAddress = 0x1234;
         const address = 0x0076;
         const operation = new AddressOperationStub(cpu);
-        const addressing = new ZeroPageAddressing(cpu, operation);
+        const addressing = new ZeroPageIndexedYAddressing(cpu, operation);
         cpu.programCounter = programAddress;
         cpu.memory.write(programAddress, ByteHelper.clipByte(address));
         const value = 130;
         operation.setValueToBeWritten(value);
+        cpu.registerY = 0x12;
+        const expected = 0x0088;
         // Act
         addressing.execute();
         // Assert
         const actual = operation.lastAddress;
-        expect(actual).toBe(address);
-        expect(cpu.memory.read(address)).toBe(value);
+        expect(actual).toBe(expected);
+        expect(cpu.memory.read(expected)).toBe(value);
         // Cleanup
         cpu.reset();
         cpu.memory.write(programAddress, 0);
         cpu.memory.write(address, 0);
+        cpu.memory.write(expected, 0);
     });
 });
